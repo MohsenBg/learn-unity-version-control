a22c928ad5a05a1c2fcc1e65a88ec2f1825d765e
Bagheri <moh.1380.1393@gmail.com>
2022-05-23 21:27:48 +0430

    t6

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static MainManager Instance;
+    public Color TeamColor;
+    // Update is called once per frame
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
